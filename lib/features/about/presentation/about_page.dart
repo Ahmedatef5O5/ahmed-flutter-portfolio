1f9b437ab32c4076341acc8087403762de7659d4
Ahmedatef5O5 <ahmedateif11@gmail.com>
2026-06-21 05:47:06 +0300

    Rename AboutPage file for improved organization and set primary scroll behavior

--- lib/features/about/presentation/about_page.dart
+++ lib/features/about/presentation/about_page.dart
@@ -14,6 +14,7 @@ class AboutPage extends StatelessWidget {
     final padding = r.value(mobile: 20.0, desktop: 60.0);
 
     return SingleChildScrollView(
+      primary: true,
       child: Center(
         child: ConstrainedBox(
           constraints: const BoxConstraints(maxWidth: AppSizes.maxContentWidth),
